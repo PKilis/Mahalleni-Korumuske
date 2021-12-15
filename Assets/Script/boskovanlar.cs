ecc66cffc99276f4f2414227707c6d4771b2bc06
Yusuf <Polat_tmnt@hotmail.com>
2021-12-15 21:18:25 +0300

    First

---
+++ Assets/Script/boskovanlar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boskovanlar : MonoBehaviour
+{
+    public AudioSource yereDusmeSesi;
+    void Start()
+    {
+        yereDusmeSesi = GetComponent<AudioSource>();
+       // Destroy(gameObject, 4f);
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Yol"))
+        {
+            yereDusmeSesi.Play();
+            Destroy(gameObject, 4f);
+        }
+    }
+}
